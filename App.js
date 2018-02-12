6e78d76c79761f7bb1103d1c3cb971555bd097d3
fafafafuse <rhazic521@gmail.com>
2018-02-12 12:35:24 +0700

    Enter

--- App.js
+++ App.js
@@ -9,20 +9,26 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state = {text:''};
+  }
   render() {
     return (
       <View style={styles.container}>
         <TextInput
           style={{height:40,
+          width: 150,
           borderColor:'gray',
           borderWidth:1}}
           placeholder="Type here to translate!"
-          onChangeText={(text) ==> this.setState({text})}
+          onChangeText={(text) => this.setState({text})}
           onSubmitEditing = {() => console.log(this.state.text)}
           vale = {this.state.text}
           />   
