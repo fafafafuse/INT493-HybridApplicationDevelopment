86cd34e613c2255b478a5a250d447b709779f8fa
fafafafuse <rhazic521@gmail.com>
2018-02-19 11:39:58 +0700

    add bg

--- WeatherProject.js
+++ WeatherProject.js
@@ -4,38 +4,43 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
   TextInput,
-  View
-} from 'react-native';
-
+  View,
+  ImageBackground
+} from "react-native";
 
 export default class WeatherProject extends Component {
   constructor(props) {
     super(props);
     this.state = { zip: "" };
-    console.log("zip "+this.state.zip) ;
+    console.log("zip " + this.state.zip);
   }
 
   _handleTextChange = event => {
     this.setState({ zip: event.nativeEvent.text });
-    console.log("onSubmit") ;
+    console.log("onSubmit");
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          You input {this.state.zip}.
-        </Text>
-        <TextInput
-          style={styles.input}
-          onSubmitEditing={this._handleTextChange}
-        />
+      <View style={styles.frame}>
+        <ImageBackground
+          source={require("./flowers.png")}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.container}>
+            <Text style={styles.welcome}>You input {this.state.zip}.</Text>
+            <TextInput
+              style={styles.input}
+              onSubmitEditing={this._handleTextChange}
+            />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -44,22 +49,22 @@ export default class WeatherProject extends Component {
 const styles = StyleSheet.create({
   frame: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
   },
   input: {
     fontSize: 20,
@@ -67,9 +72,9 @@ const styles = StyleSheet.create({
     padding: 2,
     height: 40,
     width: 100,
-    textAlign: 'center',
+    textAlign: "center"
   },
   backgroundImage: {
-      flex: 1,
-  },
+    flex: 1
+  }
 });
