f5765dc4f8c4e472fbb33201475ee21bce05ca95
fafafafuse <rhazic521@gmail.com>
2018-03-04 10:36:37 +0700

    use Nbase on App.js

--- App.js
+++ App.js
@@ -5,26 +5,23 @@
  */
 
 import React, { Component } from "react";
+import {Container,Header, Content, List, ListItem, Text, Title} from "native-base";
+
 import ContactData from "./ContactData";
 import ContactItem from "./ContactItem";
-import {
-  StyleSheet,
-  View,
-  FlatList,
-} from "react-native";
 
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = { data:[]}
+    this.state = { item:[]}
     ContactData.fetchContacts()
       .then(contacts=>{console.log(contacts);
-      this.setState({data:contacts});})
+      this.setState({item:contacts});})
       .catch(error=>{console.log(error)});
   }
 
-  _renderContact = ({ item }) => {
+  _renderContact = ( item ) => {
     const picsrc = item.firstname.toLowerCase() + ".jpeg";
     const imgurl = `http://web.sit.kmutt.ac.th/sanit/int493/contacts/img/${picsrc}`;
     return (
@@ -32,22 +29,18 @@ export default class App extends Component<Props> {
     );
   };
 
+
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList 
-        data={this.state.data} 
-        renderItem={this._renderContact} 
-        keyExtractor={(item,index) => index.toString()}/>
-      </View>
+      <Container>
+        <Header style={{paddingTop:12}}><Title>Lecturer Contact List</Title></Header>
+        <Content>
+          <List 
+          dataArray={this.state.item}
+          renderRow={this._renderContact}>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    paddingTop:20,
-    justifyContent: "center",
-    alignItems: "stretch",
-    backgroundColor: "#F5FCFF"
-  },
-});
